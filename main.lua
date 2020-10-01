4cf96bc4ea0333c373ff1555969115c986ab5acc
Ron Tseytlin <ronts390@gmail.com>
2020-10-01 17:37:59 +0300

    optionally display V in hex

--- main.lua
+++ main.lua
@@ -30,6 +30,8 @@ end
 
 local activeOnInit = true
 
+local VHex = false
+
 local currentRomData
 
 local windows = {
@@ -127,8 +129,11 @@ local windows = {
     draw = function()
       for i=0, 15 do
         if i > 0 then imgui.SameLine(0, 15) end
-        imgui.Text(("V%X\n%d"):format(i, chip8.V[i]))
+        imgui.Text(("V%X\n" .. (VHex and "%x" or "%d")):format(i, chip8.V[i]))
       end
+      
+      imgui.SameLine()
+      VHex = imgui.Checkbox("Hex##V", VHex)
       
       imgui.Separator()
       imgui.Text(("PC\n%.3x"):format(chip8.PC))
